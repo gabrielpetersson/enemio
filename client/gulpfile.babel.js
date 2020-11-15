64b9a366382b2bf96f05bce398df48ddb4e7ae8b
Gabriel Petersson <gabriielpetersson@gmail.com>
2020-11-15 20:26:49 +0100

    init

---
+++ client/gulpfile.babel.js
@@ -0,0 +1,25 @@
+import gulp from "gulp"
+import babel from "gulp-babel"
+import typescript from "gulp-typescript"
+import { createRequire } from "module"
+
+const require = createRequire(import.meta.url)
+
+const src = "./src/**/*.ts"
+const out = "./build"
+const project = typescript.createProject("tsconfig.json", {
+  outDir: out,
+  typescript: require("typescript")
+})
+
+gulp.task('build', function () {
+  const tsResult = project.src()
+    .pipe(project());
+  return tsResult.js.pipe(babel({ presets: ['es2015'] }).pipe(gulp.dest(out)))
+})
+
+gulp.task("watch", gulp.series("build", function () {
+  gulp.watch(src, gulp.series("build"))
+}))
+
+gulp.task("default", gulp.series("build"))
